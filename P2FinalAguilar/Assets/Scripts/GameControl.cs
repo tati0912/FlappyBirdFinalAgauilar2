094429d4b6c957c2935a1031f38a9138742ea504
tati0912 <730124@student.dusd.net>
2024-12-14 17:21:10 -0800

    video 7 just started the video
    3 more videos to finish

--- P2FinalAguilar/Assets/Scripts/GameControl.cs
+++ P2FinalAguilar/Assets/Scripts/GameControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -20,8 +21,12 @@ public class GameController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()  {
-        
+    void Update()
+    {
+        if(gameOver == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
     
     public void BirdDied()
